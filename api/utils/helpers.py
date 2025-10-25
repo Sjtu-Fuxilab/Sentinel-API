382bd1e9eed5aaed9b2e1e4c09cc4eae6ebde650
Sanwal Ahmad Zafar <SanwalAhmadZafar@users.noreply.github.com>
2025-10-25 09:03:15 +0000

    Add analysis notebook and utility functions

---
+++ api/utils/helpers.py
@@ -0,0 +1,13 @@
+"""
+Helper utilities
+"""
+import pandas as pd
+import numpy as np
+
+def calculate_metrics(y_true, y_pred):
+    """Calculate evaluation metrics."""
+    pass
+
+def format_response(prediction):
+    """Format prediction response."""
+    return prediction
